cf516150ccf42dc6ce866c9376688583eb3aac38
rvaldez <rvaldez@EO-5NLQPL3>
2023-05-09 20:38:49 -0500

    react-router-dom setup and new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 
 import './App.css';
+import { createBrowserRouter,createRoutesFromElements,Route, RouterProvider,Link,Outlet } from 'react-router-dom';
+import Comute from './components/Comute'
+import Search from './components/Search'
+import Home from './components/Home ';
+
 
 function App() {
-  return (
-    <div className="App">
-          <h1>Perrin</h1>
-    </div>
+
+  const Root = () => {
+    return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="comute">Comute</Link>
+        <Link to="search">Search</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+    )
+
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route exact path='/' element={<Root/>} >
+         <Route exact index element={<Home />} />
+        <Route exact path='/comute' element={<Comute />} />
+        <Route exact path='/search' element={<Search />} />
+      </Route>  
+    )
   );
+
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+  </div>
+  );
+
+
+
 }
 
 export default App;
---
+++ src/components/Comute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Comute() {
+  return (
+    <div>
+        <h1 style={{color:'red'}}>Comute</h1>
+    </div>
+  )
+}
+
+export default Comute
